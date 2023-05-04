340f6506c6ad35d108f88288391753e2aa4bedeb
Miky <vyasmiky@gmail.com>
2023-05-03 23:25:55 -0400

    added lottie sticker packs

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
-    "time"
-	"log"
-//	"golang.org/x/term"
+	"time"
+
+	//	"golang.org/x/term"
 	"github.com/joho/godotenv"
 )
 
@@ -27,7 +28,7 @@ func getrequest(url string, token string) []byte {
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
-	if err != nil{
+	if err != nil {
 		fmt.Print("error in readall")
 	}
 	return body
@@ -37,45 +38,73 @@ func getrequest(url string, token string) []byte {
 func main() {
 
 	type Server []struct {
-		ID          string   `json:"id"`
-		Name        string   `json:"name"`
-		Icon        string   `json:"icon"`
-		Owner       bool     `json:"owner"`
-		Permissions string   `json:"permissions"`
+		ID          string `json:"id"`
+		Name        string `json:"name"`
+		Icon        string `json:"icon"`
+		Owner       bool   `json:"owner"`
+		Permissions string `json:"permissions"`
 	}
 
 	type Emojis []struct {
-		Name          string        `json:"name"`
-		ID            string        `json:"id"`
-		RequireColons bool          `json:"require_colons"`
-		Managed       bool          `json:"managed"`
-		Animated      bool          `json:"animated"`
-		Available     bool          `json:"available"`
+		Name          string `json:"name"`
+		ID            string `json:"id"`
+		RequireColons bool   `json:"require_colons"`
+		Managed       bool   `json:"managed"`
+		Animated      bool   `json:"animated"`
+		Available     bool   `json:"available"`
 	}
 
-	type Stickers []struct {
-		ID          string `json:"id"`
-		Name        string `json:"name"`
-		FormatType  int    `json:"format_type"`
+	type StickerList []struct {
+		ID         string `json:"id"`
+		Name       string `json:"name"`
+		FormatType int    `json:"format_type"`
+	}
+
+	type StickerPackList struct {
+		StickerPacks []struct {
+			ID       string `json:"id"`
+			Stickers StickerList `json:"stickers"`
+			Name string `json:"name"`
+		} `json:"sticker_packs"`
 	}
 
 	err := godotenv.Load()
-    if err != nil {
+	if err != nil {
 		log.Fatal("Error loading .env file")
-    }
+	}
 	//hidden, err := term.ReadPassword(int(os.Stdin.Fd()))
-	if err != nil{
+	if err != nil {
 		fmt.Print(err)
 	}
 	//fmt.Print(string(hidden))
 	token := string(os.Getenv("AUTH"))
 	base_url := "https://discord.com/api/v10"
+	var result []map[string]string
+
+	stickerpackurl := fmt.Sprintf(base_url + "/sticker-packs")
+	stickerpackbody := getrequest(stickerpackurl, token)
+
+	var stickerpacklist StickerPackList
+	error := json.Unmarshal(stickerpackbody, &stickerpacklist)
+	if error != nil {
+		fmt.Print("error in json marshall stickerpack list ")
+		fmt.Print(error)
+	}
+	for _, stickerpack := range stickerpacklist.StickerPacks {
+		for _, sticker := range stickerpack.Stickers {
+			if sticker.FormatType == 3 {
+				name := stickerpack.Name + " " + sticker.Name
+				url := "https://cdn.discordapp.com/stickers/" + sticker.ID + ".json"
+				result = append(result, map[string]string{"name": name, "url": url})
+			}
+		}
+	}
+
 	guildsurl := base_url + "/users/@me/guilds"
 
 	body := getrequest(guildsurl, token)
 	var serverlist Server
 	json.Unmarshal(body, &serverlist)
-	var result []map[string]string
 
 	for _, server := range serverlist {
 		emojiurl := fmt.Sprintf(base_url+"/guilds/%v/emojis", server.ID)
@@ -105,8 +134,8 @@ func main() {
 		time.Sleep(1 * time.Second)
 		stickerbody := getrequest(stickerurl, token)
 
-		var stickerlist Stickers
-		err2 := json.Unmarshal(stickerbody, &stickerlist)		
+		var stickerlist StickerList
+		err2 := json.Unmarshal(stickerbody, &stickerlist)
 		if err2 != nil {
 			fmt.Print("error in json marshall sticker " + server.ID)
 			fmt.Print(err2)
@@ -119,13 +148,21 @@ func main() {
 			if sticker.FormatType == 1 {
 				tempmap["name"] = sticker.Name
 				tempmap["url"] = "https://cdn.discordapp.com/stickers/" + sticker.ID + ".png"
-			    tempstickerresult = append(tempstickerresult, tempmap)
+				tempstickerresult = append(tempstickerresult, tempmap)
+				continue
 			}
-			
+			//}else if sticker.FormatType == 4 {
+			if sticker.FormatType == 2 {
+				tempmap["name"] = sticker.Name
+				tempmap["url"] = "https://cdn.discordapp.com/stickers/" + sticker.ID + ".png"
+				tempstickerresult = append(tempstickerresult, tempmap)
+			}
+
 		}
 		result = append(result, tempstickerresult...)
 
 	}
+
 	jsonstr, _ := json.Marshal(result)
 
 	f, _ := os.Create("data.json")
